15e38a8056da399df88c7b5cc50558f909b0d70f
Aseel-Sabri <aseel.k.sabri@gmail.com>
2023-12-16 17:11:49 +0200

    Use LINQ deferred execution approach

---
+++ NumberProc/Program.cs
@@ -0,0 +1,43 @@
+﻿int[] array = { 1, 2, 3 };
+var numberProc = new NumberProc(array);
+
+numberProc
+    .Add(3)
+    .Sub(2)
+    .Div(2);
+
+numberProc.Print();
+
+
+public class NumberProc
+{
+    private IEnumerable<int> _numbers;
+
+    public NumberProc(IEnumerable<int> initialNumbers)
+    {
+        _numbers = new List<int>(initialNumbers);
+    }
+
+    public NumberProc Add(int value)
+    {
+        _numbers = _numbers.Select(num => num + value);
+        return this;
+    }
+
+    public NumberProc Sub(int value)
+    {
+        _numbers = _numbers.Select(num => num - value);
+        return this;
+    }
+
+    public NumberProc Div(int value)
+    {
+        _numbers = _numbers.Select(num => num / value);
+        return this;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine(string.Join(", ", _numbers));
+    }
+}
